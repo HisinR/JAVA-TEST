84c40ff4db312d7a209c41dc55ace40eab8d892c
hisin <1214551498@qq.com>
2019-05-02 20:10:52 +0800

    日常更新

--- src/main/java/cn.hisin.arithmetic/Main.java
+++
@@ -1,8 +0,0 @@
-package cn.hisin.arithmetic;
-
-/**
- * @author 胡海鑫
- * @date 2019/3/6 13:09
- */
-public class Main {
-}
--- src/main/java/cn.hisin.arithmetic/callback/CallbackMain.java
+++
@@ -1,9 +0,0 @@
-package cn.hisin.arithmetic.callback;
-
-public class CallbackMain {
-
-    public static void main(String[] args) throws InterruptedException {
-        SonCallbackEat sonCallbackEat = new SonCallbackEat(new Mom());
-        sonCallbackEat.askMom("饭好了没");
-    }
-}
